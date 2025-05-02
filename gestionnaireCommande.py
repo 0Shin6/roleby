222833d796b4a41560d8f714e367cbb173165a2f
Mathis Granier <mathis.granier@etu.unilim.fr>
2025-05-02 12:05:38 +0200

    WIP - création de la commande sondage

--- gestionnaireCommande.py
+++ gestionnaireCommande.py
@@ -1,5 +1,44 @@
 import discord
 from discord.ext import commands
+import asyncio
 
-async def gestionnaireCommande():
-    #gestionnaire de commande 
+class GestionnaireCommande(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="sondage")
+    #commande !sondage
+    async def sondage(self, ctx):
+        def check(msg):
+            return msg.author == ctx.author and msg.channel == ctx.channel
+        
+        try:
+            #écriture du titre du sondage
+            await ctx.send("Quel est le titre du sondage ?")
+            sujetMessage =await self.bot.wait_for('message', check=check, timeout=80)
+            sujet = sujetMessage.content
+
+            #écriture des options du sondage
+            await ctx.send("Entrez les options du sondage (séparées par des virgules `,`).")
+            optionMessage = await self.bot.wait_for('message', check=check, timeout=80)
+            options = [opt.strip() for opt in optionMessage.content.split(',') if opt.strip()]
+
+            if len(options) < 2:
+                await ctx.send("il doit y avoir au minimum 2 options et au maximum 10.")
+                return None
+
+            #écriture des émojies associés aux options
+            await ctx.send("Donnez les emojies à utiliser pour les sondages (séparés par des espaces).")
+            emojiMessage = await self.bot.wait_for('message', check=check, timeout=60)
+            emojis = emojiMessage.content.split()
+
+            if len(emojis) != len(options):
+                await ctx.send("Le nombre d'emojis ne correspond pas au nombre d'options.")
+                return None
+        #écriture de la durée du sondage (en seconde)
+        await ctx.send("Quelle est la durée du sondage ? *en seconde*")
+        dureeMessage = await self.bot.wait_for('message', check=check, timeout=45)
+        duree = int(dureeMessage)
+
+
+#Commandes : !sondage
